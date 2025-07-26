823555856d8b3a989646433fe6e1d64e85effec9
Rohit Raj <rajrohitraj392@gmail.com>
2025-07-26 17:12:58 +0530

    2nd

--- src/Components/HeroSection/HeroMain.jsx
+++ src/Components/HeroSection/HeroMain.jsx
@@ -4,7 +4,7 @@ import HeroPic from './HeroPic'
 
 const HeroMain = () => {
   return (
-    <div className='pt-52 pb-36'>
+    <div className='pt-32 pb-36'>
     <div className='flex md:flex-row sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative px-4'>
         <HeroText/>
         <HeroPic/>
--- src/Components/HeroSection/HeroPic.jsx
+++ src/Components/HeroSection/HeroPic.jsx
@@ -11,8 +11,8 @@ const HeroPic = () => {
       whileInView='show'
       viewport = {{once: false, amount:0}}
       className='h-full flex items-center justify-center'>
-      <img className='max-h-[450px] width-auto'
-      src='../../public/images/icon_profile.png'/>
+      <img className='max-h-[400px] width-auto bg-cyan opacity-50 hover:scale-125 hover:opacity-100 transition-all duration-300'
+      src='../../public/images/HeroImg.png'/>
       <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
       <PiHexagonThin  className='md:h-[90%] sm:h-[120%] min-h-[350px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]'  />
       </div>
